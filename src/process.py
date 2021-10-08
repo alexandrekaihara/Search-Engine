d73c028d562552a0bb59cc801f7667f58d54139b
Alexandre Mitsuru Kaihara <39930655+mdewinged@users.noreply.github.com>
2021-10-07 21:00:06 -0300

    a
    a

---
+++ src/process.py
@@ -0,0 +1,20 @@
+from json import load
+
+
+
+class ReverseIndex():
+    # Brief:
+    # Param:
+    # Return:
+    def __init__(self) -> None:
+        pass
+
+    def 
+
+
+
+
+
+
+if __name__ == 'main':
+    
